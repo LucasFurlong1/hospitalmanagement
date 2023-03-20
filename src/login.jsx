82febd640219185fd29e97de020aa752ec41fd8f
Lucas Furlong <trollinc.5@gmail.com>
2023-03-20 18:26:38 -0400

    test

--- src/login.jsx
+++ src/login.jsx
@@ -1,19 +1,19 @@
 import React, { useState } from "react"
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
     const [pass, setPass] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(email);
+        console.log(username);
     }
 
     return (
         <div className="auth-form-container">
             <h2 className="auth-title">Welcome to ABC Hospital</h2>
             <form className="login-form" onSubmit={handleSubmit}>
-                <label htmlFor="email">Email: </label>
-                <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="youremail@email.com" id="email" name="email" />
+                <label htmlFor="username">Username: </label>
+                <input value={username} onChange={(e) => setUsername(e.target.value)} type="username" placeholder="username" id="username" name="username" />
                 <label htmlFor="password">Password: </label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="**********" id="password" name="password" />
                 <button type="submit">Log In</button>
