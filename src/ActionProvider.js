2592627b5b3ba72975f3d92139755f0644192397
Lucas Furlong <trollinc.5@gmail.com>
2023-03-31 16:57:09 -0400

    bot almost complete. working on error validation

--- src/ActionProvider.js
+++ src/ActionProvider.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 var usertoken = ""
+var convotoken = ""
+var prevMessage = []
+var loggedIn = false
 
 class ActionProvider {
   constructor(
@@ -19,12 +22,36 @@ class ActionProvider {
   }
 
 
-  errorHandler= () => {
+  loginErrorHandler= () => {
     const message = this.createChatBotMessage("You have already logged in!")
     this.setChatbotMessage(message)
   }
 
+  userMessageErrorHandler = () => {
+    const message = this.createChatBotMessage("I'm sorry... I don't understand. Let me reiterate!")
+    this.setChatbotMessage(message)
+    if(loggedIn===false){
+      const message = this.createChatBotMessage('Hello, enter your name like this "my name is alex"')
+      this.setChatbotMessage(message)
+    }
+    else{
+      console.log(prevMessage)
+      for(const element of prevMessage){
+        const message = this.createChatBotMessage(element.message)
+        this.setChatbotMessage(message)
+      }
+    }
+  }
+
+
+  tooYoungHandler = () => {
+    const message = this.createChatBotMessage("If you would like to restart, log out and log back in!")
+    this.setChatbotMessage(message)
+  }
+
   loginHandler = (props) => {
+    prevMessage = []
+    this.state = {loginID: Math.random()}
     const options = {
       method: 'POST',
       headers: {
@@ -33,9 +60,12 @@ class ActionProvider {
         Authorization: 'token QTXDVGCtl1wehAZMgtr37hd8g9PkGKjb.dJlfIWZiyV8cNCW0hvJbOH7ujTzB1r6B',
         'x-api-key': 'gWdQEFh7265IsEBsHWtYP3dwUBtHZ9017ngbQm4m'
       },
-      body: JSON.stringify({id: 'testtest', name: props, email: 'string', email_verified: true})
+      body: JSON.stringify({id: this.state.loginID.toString(), name: props, email: 'string', email_verified: true})
     };
     
+
+    console.log(options.body)
+
     fetch('https://portal.your.md/v4/login', options)
       .then(response => response.json())
       .then((response) => {
@@ -44,11 +74,14 @@ class ActionProvider {
       })
       .catch(err => console.error(err));
 
-    const message = this.createChatBotMessage("Logged In! Hi " + props + " message 'start' to start")
+    const message = this.createChatBotMessage("Logged In! Hi, " + props + " message 'start' to start")
     this.setChatbotMessage(message)
+    prevMessage.push(message)
+    loggedIn = true
   }
 
-  chatHandler = () => {
+  initChatHandler = (props) => {
+    prevMessage = []
     const options = {
       method: 'POST',
       headers: {
@@ -57,13 +90,54 @@ class ActionProvider {
         Authorization: usertoken,
         'x-api-key': 'gWdQEFh7265IsEBsHWtYP3dwUBtHZ9017ngbQm4m'
       },
-      body: JSON.stringify({conversation_id: 'a', message: 'a'})
+      body: JSON.stringify({conversation_id: 'a', message: props})
     };
     
     fetch('https://portal.your.md/v3/chat', options)
       .then(response => response.json())
-      .then(response => console.log(response))
+      .then((response) => {
+        console.log(response)
+        let message = this.createChatBotMessage(response.messages[1].value)
+        this.setChatbotMessage(message)
+        response.question.choices.forEach(element => {
+          message = this.createChatBotMessage(element.id + " - " + element.label)
+          this.setChatbotMessage(message)
+          prevMessage.push(message)
+        });
+        convotoken = response.conversation_id
+      })
       .catch(err => console.error(err));
+  }
+
+  chatHandler = (props) => {
+    prevMessage = []
+    const options = {
+      method: 'POST',
+      headers: {
+        accept: '*/*',
+        'content-type': 'application/json',
+        Authorization: usertoken,
+        'x-api-key': 'gWdQEFh7265IsEBsHWtYP3dwUBtHZ9017ngbQm4m'
+      },
+      body: JSON.stringify({conversation_id: convotoken, message: props})
+    };
+
+    fetch('https://portal.your.md/v3/chat', options)
+    .then(response => response.json())
+    .then((response) => {
+      console.log(response)
+      response.messages.forEach(element => {
+        let message = this.createChatBotMessage(element.value)
+        this.setChatbotMessage(message)
+        prevMessage.push(message)
+      })
+      response.question.choices.forEach(element => {
+        let message = this.createChatBotMessage(element.id + " - " + element.label)
+        this.setChatbotMessage(message)
+        prevMessage.push(message)
+      })
+    })
+    .catch(err => console.error(err));
   }
 
   logoutHandler = () => {
@@ -77,12 +151,13 @@ class ActionProvider {
     console.log(options.headers.authorization)
 
     fetch('https://portal.your.md/v4/logout', options)
-      .then(response => response.json())
-      .then(response => console.log(response))
       .catch(err => console.error(err));
 
-    const message = this.createChatBotMessage("Successfully logged out!")
+    let message = this.createChatBotMessage("Successfully logged out!")
     this.setChatbotMessage(message)
+    message = this.createChatBotMessage("To log back in, please type 'my name is ' followed by your name")
+    this.setChatbotMessage(message)
+    loggedIn = false
   }
 
 
--- src/MessageParser.js
+++ src/MessageParser.js
@@ -1,4 +1,5 @@
 var login = false
+var initsymptoms = false
 
 class MessageParser {
     constructor(actionProvider, state) {
@@ -6,30 +7,154 @@ class MessageParser {
       this.state = state;
     }
 
-
     parse(message) {
 
-
+      console.log(this.state)
 
       console.log(message)
 
       const lower = message.toLowerCase()
 
+
+      var reversedMessages = this.state.messages.reverse()
+      var lastBotMessages = []
+      for(const element of reversedMessages) {
+        if(element.type==='bot'){
+          lastBotMessages.push(element)
+        }
+        if(element.type==='user'){
+          break
+        }
+      }
+
+
+
+      if(login===true && lower.includes("start")) {
+        this.actionProvider.initChatHandler(lower)
+        return
+      }
+
+
       if(lower.includes("logout")){
         this.actionProvider.logoutHandler()
         login = false
+        return
       }
 
-      if(lower.includes("my name is") && login == true){
-        this.actionProvider.errorHandler()
+      if(lower.includes("my name is") && login === true){
+        this.actionProvider.loginErrorHandler()
+        return
       }
 
-      if(lower.includes("my name is") && login == false){
-        console.log(login)
+      if(lower.includes("my name is") && login === false){
         let name = message.substring(10)
         this.actionProvider.loginHandler(name)
         login = true
+        return
       }
+
+      //if you read this next area... im sorry
+      //I could definitely do a switch here but I am too lazy to implement it 
+      login = true
+      console.log(lastBotMessages)
+      for(let i = 0; i<lastBotMessages.length; i++) {
+        if(login===true && lastBotMessages[i].message.includes(lower)){
+          console.log(i)
+          this.actionProvider.chatHandler(lower)
+          break
+        } 
+        if(login===true && lastBotMessages[i].message.includes("please tell me what symptoms") && initsymptoms===false) {
+          this.actionProvider.chatHandler(lower)
+          initsymptoms = true
+          break
+        }
+        if(lastBotMessages[i].message.includes("male or female")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("What year were you")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("What year was")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("please enter your height")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("please enter your weight")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("the name of the person")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("Please choose all the ones you have")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("How long have you had")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("Have you been diagnosed with")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("Do you think you are overweight")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("Do you smoke")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("you ever had a stroke")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("Do you have any of these symptoms")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("I'm sorry, that selection isn't")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("would you like to know")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("would help you most")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("here's some information that might be helpful")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("Was this information useful")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("first choose a category")) {
+          this.actionProvider.chatHandler(lower)
+          break
+        }
+        if(lastBotMessages[i].message.includes("I can only provide information for people over")) {
+          this.actionProvider.tooYoungHandler()
+          break
+        }
+        console.log(i)
+        if(i === lastBotMessages.length){
+          this.actionProvider.userMessageErrorHandler()
+          break
+        }
+      }
+
 
     }
   }
